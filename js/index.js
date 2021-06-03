725e8f0688649c3dc2d38b4d52d59f9314832067
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-06-03 19:12:02 +0200

    Creación de los clones + insertarlos con nombre

--- js/index.js
+++ js/index.js
@@ -3,3 +3,20 @@
 import personajes from "./personajes.js";
 
 console.log(personajes);
+
+const listaPersonajes = document.querySelector(".personajes");
+
+// Iteración array personajes
+
+for (const { nombre } of personajes) {
+  const elementoPersonaje = listaPersonajes
+    .querySelector(".personaje-dummy")
+    .cloneNode(true);
+
+  elementoPersonaje.classList.remove("personaje-dummy");
+
+  const nombrePersonaje = elementoPersonaje.querySelector(".nombre");
+  nombrePersonaje.textContent = nombre;
+
+  listaPersonajes.append(elementoPersonaje);
+}
